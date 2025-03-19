253bd2196641ef7eb726c3afbae355166ef818f6
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-03-19 10:32:26 -0300

    adicionando as rotas ao index que centraliza elas

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
 import { productRoutes } from "./products-routes";
+import { tablesRutes } from "./tables-routes";
 
 const routes = Router();
 
 routes.use("/products", productRoutes);
+routes.use("/tables", tablesRutes)
 
 
 export { routes };
