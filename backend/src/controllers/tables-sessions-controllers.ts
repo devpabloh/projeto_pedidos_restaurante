bff6ef94b9a58a36b1359b44bc46a839ba143acf
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-03-21 23:29:22 -0300

    organizando os controllers do tables-session

--- backend/src/controllers/tables-sessions-controllers.ts
+++ backend/src/controllers/tables-sessions-controllers.ts
@@ -1,9 +1,16 @@
 import {Request, Response, NextFunction } from 'express';
+import {z} from 'zod';
 
 class TablesSessionsControllers {
     async create(request: Request, response: Response, next: NextFunction){
         try {
-          return response.status(201).json()  
+            const bodySchema = z.object({
+                table_id: z.number(),
+            })
+
+            const {table_id} = bodySchema.parse(request.body)
+            
+            return response.status(201).json()  
         } catch (error) {
             next(error)
         }
