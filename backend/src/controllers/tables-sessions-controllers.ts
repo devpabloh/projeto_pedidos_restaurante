82fb57b3250189446c54384927e88ff81a00136e
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-03-21 23:31:31 -0300

    atualizando a table ssesions

--- backend/src/controllers/tables-sessions-controllers.ts
+++ backend/src/controllers/tables-sessions-controllers.ts
@@ -1,4 +1,5 @@
 import {Request, Response, NextFunction } from 'express';
+import { knex } from '@/database/knex';
 import {z} from 'zod';
 
 class TablesSessionsControllers {
@@ -10,6 +11,10 @@ class TablesSessionsControllers {
 
             const {table_id} = bodySchema.parse(request.body)
             
+            await knex("tables_sessions").insert({
+                table_id,
+            })
+
             return response.status(201).json()  
         } catch (error) {
             next(error)
