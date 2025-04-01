e43d97e27f2b1b15b0f3d11371532344aff82ab2
Pablo Henrique <pablohenriqueadm94@gmail.com>
2025-03-31 23:52:57 -0300

    Criando os controlles e adicionando as rotas do Orders

--- backend/src/routes/order-routes.ts
+++ backend/src/routes/order-routes.ts
@@ -7,5 +7,6 @@ const ordersRoutes = Router();
 const ordersController = new OrdersControllers();
 
 ordersRoutes.post("/", ordersController.create)
+ordersRoutes.get("/table-session/:table_session_id", ordersController.index)
 
 export { ordersRoutes };
